9b023a3bbe07820318e4da1ec3e57677c072de5b
Mofolabi Osotimehin <mofolabi.osotimehin@student.moringaschool.com>
2023-01-23 15:48:42 +0100

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+
+// Countdown Function
+
+function countDown(n){
+    while(n >= 0){
+        console.log(n)
+        n--;
+    }
+}
